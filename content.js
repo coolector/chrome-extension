4e677bba09f15ecc92b224cdcd194ff3691045bd
Petrica Martinescu <pmartinescu@tremend.ro>
2017-07-25 00:36:40 +0300

    Fix layout and display dropdown menu

--- content.js
+++ content.js
@@ -1,60 +1,7 @@
 'use strict';
 
-const Bloodhound = require('typeahead.js');
-// require('./css/bootstrap.css');
-// require('./css/content.css');
-// require('./js/bootstrap-tokenfield.js');
-// require('./css/bootstrap-tokenfield.css');
-// require('./css/tokenfield-typeahead.css');
-
 import MainController from './controllers/MainController';
-
 
 let mainController = new MainController();
 mainController.dispatch();
-
-/**
- * Display popup
- */
-function showPopup() {
-    let htmlPopup = require('./handlebars/popup.handlebars');
-    let popup = $('#coolector-box');
-    if (popup.eq(0).length) {
-        popup.html();
-    }
-    else {
-        $('body').append(htmlPopup());
-    }
-}
-
-showPopup();
-
-setTimeout(function () {
-    let engine = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.whitespace,
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        remote: {
-            url: 'https://private-25f9f-coolectorset.apiary-mock.com/tags?q=%QUERY%20text&limit=10&offset=0&sort=-tag%2Ccreated_at',
-            wildcard: '%QUERY',
-            transform: function(response) {
-                return response.map(function(item) {
-                    return item.tag;
-                });
-            }
-        }
-
-    });
-
-    engine.initialize();
-
-    $('#coolector-tags').tokenfield({
-        typeahead: [null, { source: engine.ttAdapter() }]
-    }).on('tokenfield:createdtoken', function (e) {
-        console.log(e.attrs.value);
-    }).on('tokenfield:removedtoken', function (e) {
-        alert('Token removed! Token value was: ' + e.attrs.value)
-    }).on('tokenfield:editedtoken', function (e) {
-        console.log("Edited" + e.attrs.value);
-    });
-}, 200);
 
--- views/MainView.js
+++ views/MainView.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import '../css/main.css';
+import '../js/bootstrap-tokenfield.js';
 
 /**
  * MainView
@@ -8,24 +9,90 @@ import '../css/main.css';
 export default class MainView {
 
     constructor() {
-
+        this.element = null;
+        this.timeout = null;
     }
 
     display() {
         let htmlPopup = require('../handlebars/popup.handlebars');
-        let popup = $('#coolector-box');
-        if (!popup.eq(0).length) {
+        this.element = $('#coolector-box');
+        if (!this.element.eq(0).length) {
             $('body').append(htmlPopup());
-            popup = $('#coolector-box');
+            this.element = $('#coolector-box');
+
+            this.initTypeahead();
         }
 
-        setTimeout(function () {
-            popup.removeClass('coolector-hidden');
+        let menu = $('#coolector-menu', this.element);
+
+        $('#coolector-menu-toggler', this.element).click((event) => {
+            menu.slideToggle('fast');
+
+            event.stopPropagation();
+        });
+
+        /**
+         * Make popup visible
+         */
+        setTimeout(() => {
+            this.element.removeClass('coolector-hidden');
         }, 200);
 
-        setTimeout(function () {
-            popup.addClass('coolector-hidden');
-        }, 3000)
+        this.initTimeout();
+        this.element.mouseleave(() => {
+            this.initTimeout();
+        });
+
+        this.element.mouseenter(() => {
+            // Stop hiding the popup
+            if (this.timeout) {
+                clearTimeout(this.timeout);
+                this.timeout = null;
+            }
+        });
+
+        this.element.click(() => {
+            menu.slideUp();
+        });
+    }
+
+    initTimeout() {
+        if (this.timeout) return;
+        // Hide popup
+        this.timeout = setTimeout(() => {
+            this.element.addClass('coolector-hidden');
+        }, 3000);
+    }
+
+    initTypeahead() {
+        const Bloodhound = require('typeahead.js');
+
+        let engine = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.whitespace,
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: {
+                url: 'https://private-25f9f-coolectorset.apiary-mock.com/tags?q=%QUERY%20text&limit=10&offset=0&sort=-tag%2Ccreated_at',
+                wildcard: '%QUERY',
+                transform: function(response) {
+                    return response.map(function(item) {
+                        return item.tag;
+                    });
+                }
+            }
+
+        });
+
+        engine.initialize();
+
+        $('#coolector-tags', this.element).tokenfield({
+            typeahead: [null, { source: engine.ttAdapter() }]
+        }).on('tokenfield:createdtoken', function (e) {
+            console.log(e.attrs.value);
+        }).on('tokenfield:removedtoken', function (e) {
+            alert('Token removed! Token value was: ' + e.attrs.value)
+        }).on('tokenfield:editedtoken', function (e) {
+            console.log("Edited" + e.attrs.value);
+        });
     }
 
     /**
